fc6d1b7723015417b14dc367b19f711828281158
Muhammad Shahmeer <24100237@lums.edu.pk>
2024-02-21 17:02:51 +0500

    builder fix

---
+++ builder/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import "./App.css"
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Product from "./components/pages/product";
+import Main from "./components/customizers/FielderMain"
+
+function App() {
+
+  return (
+    <Main />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Product />} />
+    //     <Route path="/fielder" element={<Main />} />
+    //     <Route path="/firstbase"/>
+    //     <Route path="/catcher"/>
+    //   </Routes> 
+    // </BrowserRouter>
+  );
+}
+
+export default App;
